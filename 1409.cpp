b73a9e8929a32de0ee177c5e9505a577c09c05c2
eskendir2006 <59607469+eskendir2006@users.noreply.github.com>
2020-01-07 19:14:49 +0600

    upd
    + Timus Task 1409

---
+++ 1409.cpp
@@ -0,0 +1,15 @@
+			#include <bits/stdc++.h>
+			
+			using namespace std;
+			
+			int main(){
+				
+				int g,l;
+				
+				cin>>g>>l;
+				 
+				cout<<l-1<<" "<<g-1;
+				
+				return 0;
+				
+			}
