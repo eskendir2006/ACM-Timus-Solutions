545e71aa946189463a268190f2f467be7ae8d092
eskendir2006 <59607469+eskendir2006@users.noreply.github.com>
2020-01-14 17:42:22 +0600

    upd
    +Timus 1639 Task

---
+++ 1639.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int a1,b1;
+	
+	cin>>a1>>b1;
+	
+	if(a1*b1%2==0){
+		
+		cout<<"[:=[first]";
+		
+	}
+	else cout<<"[second]=:]";
+	
+	return 0;
+	
+}
